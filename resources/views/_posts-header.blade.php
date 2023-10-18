727c238f5f6f82a5f2684c6563484b749295626f
Felipe Zorzo <57020140034@unicentro.edu.br>
2023-10-18 13:36:53 -0300

    section6 search messy way

--- resources/views/_posts-header.blade.php
+++ resources/views/_posts-header.blade.php
@@ -3,9 +3,6 @@
         Latest <span class="text-blue-500">Laravel From Scratch</span> News
     </h1>
 
-    <h2 class="inline-flex mt-2">By Lary Laracore <img src="/images/lary-head.svg"
-        alt="Head of Lary the mascot"></h2>
-
     <div class="space-y-2 lg:space-y-0 lg:space-x-4 mt-4">
         <!--  Category -->
         <div class="relative lg:inline-flex bg-gray-100 rounded-xl">
@@ -51,8 +48,11 @@
         <!-- Search -->
         <div class="relative flex lg:inline-flex items-center bg-gray-100 rounded-xl px-3 py-2">
             <form method="GET" action="#">
-                <input type="text" name="search" placeholder="Find something"
-                    class="bg-transparent placeholder-black font-semibold text-sm">
+                <input type="text"
+                    name="search"
+                    placeholder="Find something"
+                    class="bg-transparent placeholder-black font-semibold text-sm"
+                    value="{{request('search')}}">
             </form>
         </div>
     </div>
