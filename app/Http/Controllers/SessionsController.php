63ab1e947cb2f857ef75697f2d7562a9f5e7be00
Felipe Zorzo <57020140034@unicentro.edu.br>
2023-10-19 11:53:54 -0300

    50 login page

--- app/Http/Controllers/SessionsController.php
+++ app/Http/Controllers/SessionsController.php
@@ -2,9 +2,39 @@
 
 namespace App\Http\Controllers;
 
+use Illuminate\Validation\ValidationException;
 
 class SessionsController extends Controller
 {
+    public function create(){
+        return view('sessions.create');
+    }
+
+    public function store(){
+        $attributes = request()->validate([
+            'email'=>['required','email'],
+            'password'=> ['required']
+        ]);
+
+        // attempt to authenticate and log in the user
+        //based on the provided credentialss
+        if (auth()->attempt($attributes)){
+            //session fixation
+            session()->regenerate();
+            // redirect with a success flash message
+            return redirect('/')->with('Success', 'Welcome Back!');
+        }
+
+        //auth failed
+        throw ValidationException::withMessages([
+            'email' => 'Your provided credential could not be verified.'
+        ]);
+
+        // return back()
+        //     ->withInput()
+        //     ->withErrors(['email' => 'Your provided credential could not be verified.']);
+    }
+
     public function destroy(){
         auth()->logout();
 
