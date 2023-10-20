60256cf414b82b11a1c5fef6d7e45d0bcffdd688
Felipe Zorzo <57020140034@unicentro.edu.br>
2023-10-20 10:45:52 -0300

    section11 end

--- app/Http/Controllers/NewsletterController.php
+++ app/Http/Controllers/NewsletterController.php
@@ -5,7 +5,6 @@ namespace App\Http\Controllers;
 use App\Services\Newsletter;
 use Exception;
 use Illuminate\Validation\ValidationException;
-use Illuminate\Http\Request;
 
 class NewsletterController extends Controller
 {
@@ -20,7 +19,6 @@ class NewsletterController extends Controller
                 'email' => 'This email could not be added to our newsletter list.'
             ]);
         }
-
 
         return redirect('/')-> with('Success', 'You are now signed up for our newsletter!');
         }
--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,9 +2,11 @@
 
 namespace App\Providers;
 
+use App\Services\MailchimpNewsLetter;
+use App\Services\Newsletter;
 use Illuminate\Database\Eloquent\Model;
-use Illuminate\Pagination\Paginator;
 use Illuminate\Support\ServiceProvider;
+use MailchimpMarketing\ApiClient;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -13,7 +15,14 @@ class AppServiceProvider extends ServiceProvider
      */
     public function register(): void
     {
-        //
+        app()->bind(Newsletter::class, function(){
+            $client = (new ApiClient)->setConfig([
+                'apiKey' => config('services.mailchimp.key'),
+                'server' => 'us14'
+            ]);
+
+            return new MailchimpNewsLetter($client);
+        });
     }
 
     /**
