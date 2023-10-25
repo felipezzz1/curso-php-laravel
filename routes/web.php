0f737191efded7980fd918de300a03c456f50095
Felipe Zorzo <57020140034@unicentro.edu.br>
2023-10-25 13:52:38 -0300

    end

--- routes/web.php
+++ routes/web.php
@@ -24,6 +24,7 @@ Route::post('login', [SessionsController::class, 'store'])->middleware('guest');
 Route::post('logout', [SessionsController::class, 'destroy'])->middleware('auth');
 
 Route::middleware('can:admin')->group(function(){
+    // Route::resource('admin/posts', AdminPostController::class)->except('show');
     Route::post('admin/posts', [AdminPostController::class, 'store']);
     Route::get('admin/posts', [AdminPostController::class, 'index']);
     Route::get('admin/posts/{post}/edit', [AdminPostController::class, 'edit']);
